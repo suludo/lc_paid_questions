e9d57fc1e7c7d07044b9824cc8d214d6ae65595b
Xiaorui Zhao <xiaoruizhao@Xiaoruis-MacBook-Pro.local>
2016-02-22 23:05:55 -0600

    reverse word in string ii implementationa added.

--- LC_paid_questions/ReverseWordsInStringII.hpp
+++ LC_paid_questions/ReverseWordsInStringII.hpp
@@ -9,5 +9,31 @@
 #ifndef ReverseWordsInStringII_h
 #define ReverseWordsInStringII_h
 
+/*
+http://www.cnblogs.com/EdwardLiu/p/4306561.html
+ 
+Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+
+The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Could you do it in-place without allocating extra space?
+ */
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void reverseWords(string &s) {
+    reverse(s.begin(), s.end());
+    for (int i = 0, j = 0; i < s.size(); i = j + 1) {
+        for (j = i; j < s.size() && !isblank(s[j]); ++j);
+        reverse(s.begin()+i, s.begin()+j);
+    }
+}
 
 #endif /* ReverseWordsInStringII_h */
