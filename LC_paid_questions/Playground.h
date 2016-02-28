3fbe67bf6a9c75d8a49082d6f809ff0f6672a001
raymon <xraymonzhao@gmail.com>
2016-02-27 22:24:03 -0600

    add Playground to test any ideas i happen to have

---
+++ LC_paid_questions/Playground.h
@@ -0,0 +1,13 @@
+//
+//  Playground.h
+//  LC_paid_questions
+//
+//  Created by Xiaorui Zhao on 2/27/16.
+//  Copyright © 2016 Xiaorui Zhao. All rights reserved.
+//
+
+#ifndef Playground_h
+#define Playground_h
+
+
+#endif /* Playground_h */
